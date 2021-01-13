112e390ee386621e5ee5822f8e84027af6884f82
Wesley <wspencer3295@yahoo.com>
2021-01-13 16:47:23 -0700

    commit tree a26b2f5b0620bd83f73a62169ba44a1ddf5e7bb2

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -6,12 +6,31 @@ class AuthorsController < ApplicationController
   def new
     @author = Author.new
   end
-
+  
   def create
-    @author = Author.create(author_params)
-
-    redirect_to author_path(@author)
+    @author = Author.new(author_params)
+    if @author.valid?
+      @author.save
+      redirect_to author_path(@author)
+    else
+      render :new
+    end
   end
+  def edit
+    @author = Author.find(params[:id])
+  end
+
+  def update
+    @author = Author.find(params[:id])
+    if @author.update(author_params)
+      @author.save
+      redirect_to author_path(@author)
+    else
+      render :edit
+
+    end
+  end
+  
 
   private
 
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,18 +1,37 @@
 class PostsController < ApplicationController
-  def show
-    @post = Post.find(params[:id])
+  
+  def new
+    @post = Post.new
   end
-
+  
+  def create
+    @post = Post.new(post_params)
+    if @post.valid?
+      @post.save
+      redirect_to post_path(@post)
+    else
+      render :new
+    end
+  end
+  
+  
+  
   def edit
     @post = Post.find(params[:id])
   end
-
+  
   def update
     @post = Post.find(params[:id])
-
-    @post.update(post_params)
-
-    redirect_to post_path(@post)
+    if @post.update(post_params)
+      @post.save
+      redirect_to post_path(@post)
+    else
+      render :edit
+      
+      def show
+        @post = Post.find(params[:id])
+      end
+    end
   end
 
   private
